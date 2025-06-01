dfe73e02ba09a4924701cf58f7c007d19cf3ea97
levvedrov <levvedrov@g.skku.edu>
2025-06-01 15:21:04 +0300

    train script optimized

--- train_script.py
+++ train_script.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import math
 import os
 import random
+import time
 
 learningRate = 0.005
 epocheNum = 10000
@@ -17,38 +18,30 @@ er0 = epocheError
 
 errorslst = []
 epochelst = []
-fig, ax = plt.subplots() 
-ax.set_xlabel('epoch')
-ax.set_ylabel('sqError')
-ax.set_title('Training Error per Epoch')
-ax.grid()
-plt.show(block=False)
-
 
 
 for epoche in range(0,epocheNum):
     epocheError = 0
     print(f"Epoche : {epoche}")
 
-
-
     for i in range(0,10):
         pngIndex = int(random.uniform(0,99))
         Errors = a.learn("model-1", "learning_dataset", i, learningRate, pngIndex)
         print(f"        Learning number {i} : idx {pngIndex}")
         epocheError += math.pow(Errors.matrix[i][0],2)
-
+    
+    time.sleep(1)
     errorslst.append(epocheError)
     epochelst.append(epoche)
-    
-
-
-    
-
     print(f"     Error = {epocheError}")
 
 
-
+fig, ax = plt.subplots() 
+ax.set_xlabel('epoch')
+ax.set_ylabel('sqError')
+ax.set_title('Training Error per Epoch')
+ax.grid()
+plt.show(block=False)
 ax.plot(epochelst, errorslst, color='red')
 plt.draw()
 plt.pause(0.001)
